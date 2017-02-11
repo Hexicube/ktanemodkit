2b86d328f30096b96f10e006cdfb90b9336b0d07
Hexicube <Hexicube@users.noreply.github.com>
2017-02-11 04:18:24 +0000

    Added component categories to missions
    This change will allow selecting a component category for a mission and have it automatically populate the modded modules field with the modules specified in the category.
    
    Stuff to do:
    - Move the category GUID list into KMMissions so that it saves onto the asset (or create a mission-category pair list)
    - Allow specifying vanilla modules
    - Detect changes to the category and apply them to missions automatically

---
+++ Assets/Editor/Scripts/Missions/ComponentCategory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ComponentCategory))]
+public class ComponentCategory : Editor {
+    public string CategoryName;
+    public string[] ComponentNames;
+
+    public override void OnInspectorGUI()
+    {
+        if (target != null)
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("CategoryName"), false);
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("ComponentNames"), true);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
